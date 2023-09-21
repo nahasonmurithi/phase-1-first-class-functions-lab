002cb51788282aaaa24b0c788ea707437d25dea3
Nahason <nahason.murithi@student.moringaschool.com>
2023-09-21 17:59:09 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){
+return drivers.slice(0, 2);
+}
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num1,){
+    return charge => charge * num1; 
+}
+const fareDoubler = (charge) => {return charge * 2}; 
+
+const fareTripler = (charge) => {return charge * 3};
+
+const selectDifferentDrivers = (returnFirstTwoDrivers, returnLastTwoDrivers) => {return returnLastTwoDrivers(returnFirstTwoDrivers)};
